636f35ee845558e963dcef761c1410d3db4d3d2a
connor-voice <connor.voice.683@gmail.com>
2021-11-08 17:07:46 +0000

    Updated and finished work

--- src/arrays/EnhancedForLoops.java
+++ src/arrays/EnhancedForLoops.java
@@ -5,13 +5,35 @@ public class EnhancedForLoops {
 	public static void main(String[] args) {
 
 		method1();
+
+		int[] stringArrayThree = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+		for (int i : stringArrayThree) {
+			if (intToBool(i)) {
+				System.out.println("Even " + i * i * i);
+			} else {
+				System.out.println("Odd " + i * i);
+			}
+		}
+
 	}
 
 	public static void method1() {
-		int[] stringArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-		
-		for(int arrayNum : stringArray) {
+		int[] stringArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+		for (int arrayNum : stringArray) {
 			System.out.println(arrayNum * arrayNum);
+		}
+	}
+
+	public static boolean intToBool(int num) {
+
+//		for (int i = 0; i < boolCheckString.length; i++) {
+//			System.out.println(boolCheckString[i] % 2 == 0);
+		if (num % 2 == 0) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
--- src/arrays/StringManipulation.java
+++ src/arrays/StringManipulation.java
@@ -4,18 +4,11 @@ public class StringManipulation {
 	public static void main(String[] args) {
 		method1();
 	}
+
 	public static void method1() {
-		String myString = "I lIke this";
-		String secondString = "I LIKE THIS";
-		
-		System.out.println(myString.toLowerCase());
-		
-		System.out.println(myString.toLowerCase().contains("ik")); // change to lower before checking contain
-		
-		System.out.println(myString.equalsIgnoreCase(secondString)); // ignore case on check
-		
-		for (int i = 0; i < myString.length(); i++) {
-			System.out.println(myString.charAt(i));
-		}
+		String rainyDay = "yesterday it was raining";
+		String sunnyDay = "today it is sunny";
+
+		System.out.println(sunnyDay.toUpperCase() + ", " + rainyDay.toUpperCase());
 	}
 }
